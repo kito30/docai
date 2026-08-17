68958e8ebac0c1d8c1588c8e7018956d8c0b5286
kito30 <duyanhpham30102004@gmail.com>
2026-08-18 02:39:40 +1000

    add embeded text ready for ai reading test

--- docai/src/main/java/com/github/kito/docai/domain/document/DocumentChunk.java
+++ docai/src/main/java/com/github/kito/docai/domain/document/DocumentChunk.java
@@ -1,7 +1,16 @@
 package com.github.kito.docai.domain.document;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
+import com.pgvector.PGvector;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class DocumentChunk {
@@ -18,6 +27,8 @@ public class DocumentChunk {
     @Column(columnDefinition = "TEXT")
     private String chunkText;
 
+    @Column(columnDefinition = "vector(1536") //text-embedding-3-small size
+    private PGvector embedding;
 
     public DocumentChunk() {}
 
@@ -25,12 +36,14 @@ public class DocumentChunk {
         this.document = document;
         this.chunkText = chunkText;
     }
-
+    
     public Long getId() { return id; }
     public String getChunkText() { return chunkText; }
     public Document getDocument() { return document; }
-
+    
+    public PGvector getEmbedding() {return embedding; }
     public Long getDocumentId() {
         return document != null ? document.getId() : null;
     }
+    public void setEmbedding(PGvector embedding) {this.embedding = embedding; }
 }
